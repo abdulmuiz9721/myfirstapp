fe1fc4515bca0e5b550c781c4a303ed3137f7ce1
abdulmuiz9721 <100678053+abdulmuiz9721@users.noreply.github.com>
2022-03-14 02:26:10 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -6,34 +6,50 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
-data = pd.read_table('Data.csv', index_col = False,  sep = ',', skipinitialspace = True)
-data
 
-df = data.drop(['Substitution ','xG', 'xG Per Avg Match',
-       'OnTarget',  'On Target Per Avg Match'], axis = 1)
-
-df.head()
 
 st.header("My First Streamlit App")
+st.sidebar.write("<a href='https://www.kaggle.com/dwisetyoaji/data-analysis-football-leagues/data", unsafe_allow_html=True)
 
 option = st.sidebar.selectbox(
     'Select',
-     ['1','map','T n C'])
+     ['1','2','T n C'])
+
+
+
+
+data = pd.read_table('Data.csv', index_col = False,  sep = ',', skipinitialspace = True)
+df = data.drop(['Substitution ','xG', 'xG Per Avg Match',
+       'OnTarget', 'On Target Per Avg Match'], axis = 1)
+
+
 
 
 if option=='1':
+  
     label = list(total_match_2019.index.values)
 plt.pie(total_match_2019,labels=label,autopct='%1.1f%%')
 plt.title("Total Played Match")
 plt.show()
 
-    st.line_chart(chart_data)
+    st.pie_chart(chart_data)
 
-elif option=='map':
+elif option=='2':
     map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
+    st.map(map_data)
+
+else:
+
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+        'Intplan': ['yes', 'yes', 'yes', 'no'],
+        'Churn Status': [0, 0, 0, 1]
+        }))
     st.map(map_data)
 
 else:
