e495cfe03a743d0b45e9dc5457ff8f83378d2688
abdulmuiz9721 <100678053+abdulmuiz9721@users.noreply.github.com>
2022-03-14 02:41:59 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -13,43 +13,33 @@ st.sidebar.write("<a href='https://www.kaggle.com/dwisetyoaji/data-analysis-foot
 
 option = st.sidebar.selectbox(
     'Select',
-     ['1','2','T n C'])
+     ['Players with most goals in 2019','2','T n C'])
 
 
 
-
+#select data
 data = pd.read_table('Data.csv', index_col = False,  sep = ',', skipinitialspace = True)
 df = data.drop(['Substitution ','xG', 'xG Per Avg Match',
        'OnTarget', 'On Target Per Avg Match'], axis = 1)
+df =data[(data["Year"] > 2018) & (data["Year"] < 2020)]
+df2 = df[df['League'].isin(['Premier League', 'La Liga', 'Serie A', 'Bundesliga'])]
+df3 = df2[df2["Goals"] > 15]
 
 
 
-
-if option=='1':
+if option=='Players with most goals in 2019':
   
-    label = list(total_match_2019.index.values)
-plt.pie(total_match_2019,labels=label,autopct='%1.1f%%')
-plt.title("Total Played Match")
-plt.show()
+  chart_data = pd.DataFrame(df3,columns=['Player Names','Goals'])
+  df.plot(x ='Player Names', y='Goals', kind = 'bar')
+    st.bar_chart(chart_data)
 
-    st.pie_chart(chart_data)
+    
 
 elif option=='2':
     map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
-    st.map(map_data)
-
-else:
-
-    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-    show = st.checkbox('I agree the terms and conditions')
-    if show:
-        st.write(pd.DataFrame({
-        'Intplan': ['yes', 'yes', 'yes', 'no'],
-        'Churn Status': [0, 0, 0, 1]
-        }))
     st.map(map_data)
 
 else:
