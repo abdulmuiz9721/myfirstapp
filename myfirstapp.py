c9370956ba868e0885a38c798524dff16d658438
abdulmuiz9721 <100678053+abdulmuiz9721@users.noreply.github.com>
2022-03-14 23:02:49 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 
 df=pd.read_csv("Number of Cases of Covid-19 in 2020.csv")
@@ -32,6 +32,5 @@ option = st.sidebar.selectbox(
 if option == 'bar chart': 
     df2 = pd.DataFrame(df,columns=['State','Total'])
     df.plot(x ='State', y='Total', kind = 'bar')
-    plt.show()
 st.bar_chart(df2)
 
