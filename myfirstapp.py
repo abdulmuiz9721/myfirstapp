a73caa2d25f520be1a4f2120cbe13694cb544e8f
abdulmuiz9721 <100678053+abdulmuiz9721@users.noreply.github.com>
2022-03-14 10:54:45 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -45,12 +45,7 @@ if option=='Players with most goals in 2019':
 
     
 
-elif option=='2':
-    map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
 
-    st.map(map_data)
 
 else:
 
