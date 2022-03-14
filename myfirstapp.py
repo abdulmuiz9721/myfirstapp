6f6e6a392382d193a4d106810e6ad89f10dcecd7
abdulmuiz9721 <100678053+abdulmuiz9721@users.noreply.github.com>
2022-03-14 10:52:29 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,29 +1,34 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 
 
 
 st.header("My First Streamlit App")
-st.sidebar.write("<a href='https://www.kaggle.com/dwisetyoaji/data-analysis-football-leagues/data", unsafe_allow_html=True)
+
 
 option = st.sidebar.selectbox(
     'Select',
      ['Players with most goals in 2019','2','T n C'])
 
-
-
 #select data
-data = pd.read_table('Data.csv', index_col = False,  sep = ',', skipinitialspace = True)
-df = data.drop(['Substitution ','xG', 'xG Per Avg Match',
+
+def load_data(nrows):
+    data = pd.read_table('Data.csv', index_col = False,  sep = ',', skipinitialspace = True)
+    df = data.drop(['Substitution ','xG', 'xG Per Avg Match',
        'OnTarget', 'On Target Per Avg Match'], axis = 1)
-df =data[(data["Year"] > 2018) & (data["Year"] < 2020)]
-df2 = df[df['League'].isin(['Premier League', 'La Liga', 'Serie A', 'Bundesliga'])]
-df3 = df2[df2["Goals"] > 15]
+    df =data[(data["Year"] > 2018) & (data["Year"] < 2020)]
+    df2 = df[df['League'].isin(['Premier League', 'La Liga', 'Serie A', 'Bundesliga'])]
+    df3 = df2[df2["Goals"] > 15]
+    return df3
+
+
+
+
+
+
 
 
 
@@ -31,12 +36,11 @@ if option=='Players with most goals in 2019':
   
   
 
-    chart_data = px.bar(
-        Year, 
+    chart_data = px.bar(df3, 
         x='Player Names',
         y='Goals',
         
-        st.plotly_chart(chart_data)
+        st.bar_chart(chart_data['2019_data'])
 
 
     
