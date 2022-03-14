0557849cfb4d9845fb9a6ceb6768a6a695fcb2e3
abdulmuiz9721 <100678053+abdulmuiz9721@users.noreply.github.com>
2022-03-14 22:45:08 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -29,7 +29,10 @@ option = st.sidebar.selectbox(
 
 
 if option=='bar chart':
-    chart_data = pd.DataFrame(df['State','Total']),
+    chart_data = pd.DataFrame(
+        df['State','Total']),
     columns=['State', 'Total'],
+    x='State'
+    y='Total'
     st.bar_chart(chart_data)
 
