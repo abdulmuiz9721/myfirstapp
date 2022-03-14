9807e8a9c2b13d7447053f0a0362f01856ec1b63
abdulmuiz9721 <100678053+abdulmuiz9721@users.noreply.github.com>
2022-03-14 23:47:03 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 
@@ -30,8 +30,19 @@ option = st.sidebar.selectbox(
      ['bar chart','T n C'])
 
 
-if option == 'bar chart': 
-    df2 = pd.DataFrame(df,columns=['State','Total'])
-    df.plot(x ='State', y='Total', kind = 'bar')
-st.bar_chart(df2)
+if option == 'Pie chart': 
+    State=['Johor', 'Kedah','Kelantan', 'Melaka','Negeri Sembilan', 'Pahang', 'Pulau Pinang','Perak', 'Perlis,]
 
+    Total = [814, 292, 407, 632, 1167, 474, 167, 628, 61]
+
+#The plot
+fig = go.Figure(
+    go.Pie(
+    labels = State,
+    values = Total,
+    hoverinfo = "label+percent",
+    textinfo = "value"
+))
+
+st.header("Pie chart")
+st.plotly_chart(fig)
